60531ce361bde7f8a244c880342d9acca8126f5e
ingrid-cast <130702162+ingrid-cast@users.noreply.github.com>
2023-10-23 14:59:29 -0300

    Delete matriz directory

--- matriz/guardar_proyecto.php
+++
@@ -1,9 +0,0 @@
-<?php
-include("conexion.php");
-$con=conectar();
-echo "exito ";
-$nombre=$_POST['nombre'];
-$fechainicio=$_POST['fechainicio'];
-mysqli_query($con,"INSERT INTO `proyectos` (`proyecto`, `fecha_Inicio`) VALUES ('$nombre', '$fechainicio');");
-desconectar($con);
-?>
